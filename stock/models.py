5f5059ca990ff76097c0432a3cc699d5b4e4b726
unknown <pemeraldy@gmail.com>
2020-06-09 07:03:25 +0100

    displayed other info: total order, pending and delivered on dashboard

--- stock/models.py
+++ stock/models.py
@@ -49,5 +49,5 @@ class Order(models.Model):
 
     status = models.CharField(max_length=20, null=True, choices=STATUS)
 
-    def __str__ (self):
-        return self.customer 
+    # def __str__ (self):
+    #     return self.customer 
